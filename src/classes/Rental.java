14a48fdef3e21a53393decf4dbd7981600eca341
TheLordXII <56317585+TheLordXII@users.noreply.github.com>
2020-05-14 17:03:26 +0200

    Moving the rental calculation in the rental class

--- src/classes/Rental.java
+++ src/classes/Rental.java
@@ -16,4 +16,27 @@ class Rental {
     public Movie getMovie() {
         return movie;
     }
+
+    public double getCharge() {
+        double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (rental.getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
+                break;
+            default:
+                System.out.println("No price category found.");
+                break;
+        }
+        return result;
+    }
 }
